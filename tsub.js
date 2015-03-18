7024fc73e049124f6516a79a90c71adbf70828f3
Jan Hendrik Mangold <jhm@cotren.net>
2015-03-18 14:25:02 -0700

    Initial commit

---
+++ tsub.js
@@ -0,0 +1,72 @@
+if (Meteor.isClient) {
+  var remoteUrl = 'http://mongodb-image.devel.buzzledom.com';
+
+  Session.setDefault('useNewTemplateSubscribe', true);
+  Session.setDefault('useTemplate','hello');
+  Session.setDefault('errorMessage','');
+
+  Meteor.remoteConnection = DDP.connect(remoteUrl);
+
+  DBImages = new Meteor.Collection('dbimages',{connection: Meteor.remoteConnection});
+
+  Template.registerHelper('useTemplate', function(){
+    return Session.get('useTemplate');
+  });
+
+  Template.hello.onCreated(function(){
+    Session.set('errorMessage','');
+
+    if(Session.get('useNewTemplateSubscribe'))
+      this.subscribe('thumbnails',0,{
+        connection: Meteor.remoteConnection,
+        onError: function(err) {
+          console.log(err);
+          Session.set('errorMessage', EJSON.stringify(err,{indent: true}));
+        }
+      });
+    else
+      this.dbimagesHandle = Meteor.remoteConnection.subscribe('thumbnails',0);
+  });
+
+  Template.hello.onDestroyed(function(){
+    if(this.dbimagesHandle)
+      this.dbimagesHandle.stop();
+  });
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    },
+    images: function() {
+      return DBImages.find();
+    },
+    errorMessage: function(){
+      return Session.get('errorMessage');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('useTemplate','changeMethod');
+    }
+  });
+
+  Template.changeMethod.helpers({
+    checked: function() {
+      return Session.get('useNewTemplateSubscribe');
+    }
+  });
+  Template.changeMethod.events({
+    'change input': function (e,t) {
+      Session.set('useNewTemplateSubscribe', e.target.checked);
+      Session.set('useTemplate', 'hello');
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
